da1af32d48bf046f5b1281082e7d95cba5174805
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-07-04 11:29:49 -0700

    added submodule initialization to bash.sh

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+readonly SMU_PATH="$HOME/set-me-up"
+
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing homebrew..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -8,6 +10,13 @@ else
     brew update
 fi
 
+echo "------------------------------"
+echo "Updating and/or installing submodules"
+echo "------------------------------"
+
+cd "${SMU_PATH}" \
+    && git submodule update --quiet --init --recursive \
+    && git submodule foreach git pull origin master
 
 if [[ $(command -v rcup) == "" ]]; then
 
@@ -16,7 +25,6 @@ if [[ $(command -v rcup) == "" ]]; then
 
     brew bundle install -v --file="./brewfile"
 fi
-
 
 echo "------------------------------"
 echo "Updating and/or installing dotfiles"
