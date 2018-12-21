b5f62dc9359f569362fe4afb8494f781aba26d8f
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-21 10:58:39 -0500

    updated base.sh to include bash 3.2 upgrade

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -99,6 +99,72 @@ symlink() {
 
 }
 
+change_default_bash() {
+
+    local configs=""
+    local pathConfig=""
+
+    local newShellPath=""
+    local brewPrefix=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Try to get the path of the `Bash`
+    # version installed through `Homebrew`.
+
+    brewPrefix="$(brew_prefix)" \
+        || return 1
+
+    pathConfig="PATH=\"$brewPrefix/bin:\$PATH\""
+    configs="
+# Homebrew bash configurations
+$pathConfig
+export PATH
+"
+
+    newShellPath="$brewPrefix/bin/bash" \
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Add the path of the `Bash` version installed through `Homebrew`
+    # to the list of login shells from the `/etc/shells` file.
+    #
+    # This needs to be done because applications use this file to
+    # determine whether a shell is valid (e.g.: `chsh` consults the
+    # `/etc/shells` to determine whether an unprivileged user may
+    # change the login shell for their own account).
+    #
+    # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
+
+    if ! grep "$newShellPath" < /etc/shells &> /dev/null; then
+        execute \
+            "printf '%s\n' '$newShellPath' | sudo tee -a /etc/shells" \
+            "Bash (add '$newShellPath' in '/etc/shells')" \
+        || return 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Set latest version of `Bash` as the default
+    # (macOS uses by default an older version of `Bash`).
+
+    chsh -s "$newShellPath" &> /dev/null
+    print_result $? "Bash (use latest version)"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If needed, add the necessary configs in the
+    # local shell configuration file.
+
+    if ! grep "^$pathConfig" < "$LOCAL_BASH_CONFIG_FILE" &> /dev/null; then
+        execute \
+            "printf '%s' '$configs' >> $LOCAL_BASH_CONFIG_FILE \
+                && . $LOCAL_BASH_CONFIG_FILE" \
+            "Bash (update $LOCAL_BASH_CONFIG_FILE)"
+    fi
+
+}
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
@@ -129,6 +195,12 @@ main() {
     print_in_yellow "\n   Symlink dotfiles\n\n"
 
     symlink
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    print_in_yellow "\n   Upgrade bash\n\n"
+
+    change_default_bash
 
 }
 
