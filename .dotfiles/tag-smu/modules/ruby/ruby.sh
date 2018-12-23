84f3f9f12dc2c96721c0cbd901fafdc4212cd051
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-23 14:09:03 -0500

    updated ruby.sh

--- .dotfiles/tag-smu/modules/ruby/ruby.sh
+++ .dotfiles/tag-smu/modules/ruby/ruby.sh
@@ -7,86 +7,7 @@ declare current_dir && \
     cd "${current_dir}" && \
     source "$HOME/set-me-up/.dotfiles/utilities/utils.sh"
 
-readonly SMU_PATH="$HOME/set-me-up"
-
-LOCAL_BASH_CONFIG_FILE="${HOME}/.bash.local"
-LOCAL_FISH_CONFIG_FILE="${HOME}/.fish.local"
-
-declare -r RBENV_DIRECTORY="$HOME/.rbenv"
-declare -r RBENV_GIT_REPO_URL="https://github.com/rbenv/rbenv.git"
-declare -r RUBY_BUILD_DIRECTORY="$RBENV_DIRECTORY/plugins/ruby-build"
-declare -r RUBY_BUILD_GIT_REPO_URL="https://github.com/rbenv/ruby-build.git"
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# If needed, add the necessary configs in the
-# local shell configuration files.
-add_rbenv_configs() {
-
-    # bash
-
-    declare -r BASH_CONFIGS="
-# Rbenv - Groom your app’s Ruby environment.
-export RBENV_ROOT=\"$RBENV_DIRECTORY\"
-export PATH=\"\$RBENV_ROOT/bin:\$PATH\"
-eval \"\$(rbenv init -)\""
-
-    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$BASH_CONFIGS" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$BASH_CONFIGS' >> $LOCAL_BASH_CONFIG_FILE \
-                && . $LOCAL_BASH_CONFIG_FILE" \
-            "rbenv (update $LOCAL_BASH_CONFIG_FILE)"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # fish
-
-    declare -r FISH_CONFIGS="
-# Rbenv - Groom your app’s Ruby environment.
-set -gx RBENV_ROOT $RBENV_DIRECTORY
-set -gx PATH \$PATH \$RBENV_ROOT/bin"
-
-    if [ ! -e "$LOCAL_FISH_CONFIG_FILE" ] || ! grep -q "$(<<<"$FISH_CONFIGS" tr '\n' '\01')" < <(less "$LOCAL_FISH_CONFIG_FILE" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$FISH_CONFIGS' >> $LOCAL_FISH_CONFIG_FILE" \
-            "rbenv (update $LOCAL_FISH_CONFIG_FILE)"
-    fi
-
-}
-
-install_rbenv() {
-
-    # Install `rbenv` and add the necessary
-    # configs in the local shell config files.
-
-    execute \
-        "git clone --quiet $RBENV_GIT_REPO_URL $RBENV_DIRECTORY" \
-        "rbenv (install)" \
-    && add_rbenv_configs
-
-}
-
-update_rbenv() {
-
-    if [ -d "$RBENV_DIRECTORY" ]; then
-        execute \
-            "cd $RBENV_DIRECTORY \
-                && git fetch --quiet origin" \
-            "rbenv (upgrade)"
-    fi
-
-}
-
-install_ruby_build() {
-
-    # Install `ruby-build`
-
-    execute \
-        "git clone --quiet $RUBY_BUILD_GIT_REPO_URL $RUBY_BUILD_DIRECTORY" \
-        "ruby-build (install)"
-
-}
 
 install_latest_stable_ruby() {
 
@@ -113,8 +34,7 @@ install_latest_stable_ruby() {
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     latest_version="$(
-        . "$LOCAL_BASH_CONFIG_FILE" \
-        && rbenv install -l | \
+        rbenv install -l | \
         grep -v - | \
         tail -1 | \
         tr -d '[:space:]'
@@ -128,8 +48,7 @@ install_latest_stable_ruby() {
 
     if [ "$current_version" != "$latest_version" ]; then
         execute \
-            ". $LOCAL_BASH_CONFIG_FILE \
-                && rbenv install $latest_version \
+            "rbenv install $latest_version \
                 && rbenv global $latest_version" \
             "rbenv (install ruby v$latest_version)"
     else
@@ -160,16 +79,7 @@ main() {
     printf "\n"
 
     ask_for_sudo
-
-    if [ ! -d "$RBENV_DIRECTORY" ] && ! cmd_exists "rbenv"; then
-        install_rbenv
-        install_ruby_build
-    else
-        update_rbenv
-    fi
-
-    printf "\n"
-
+    
     install_latest_stable_ruby
 
     install_ruby_gems
