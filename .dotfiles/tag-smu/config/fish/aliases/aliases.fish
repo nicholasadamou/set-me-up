3d350bb4c1c66b0398c0e543d664363ae5d2ebe6
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-12-23 23:16:52 -0500

    updated

--- .dotfiles/tag-smu/config/fish/aliases/aliases.fish
+++ .dotfiles/tag-smu/config/fish/aliases/aliases.fish
@@ -18,7 +18,7 @@ alias fs "stat -f \"%z bytes\""
 
 function du --description "Updates the dotfiles directory"
     eval $DOTFILES/smu --selfupdate
-    RCRC="$DOTFILES/.dotfiles/rcrc" rcup -v -d "$DOTFILES/.dotfiles"
+    env RCRC="$DOTFILES/.dotfiles/rcrc" rcup -v -d "$DOTFILES/.dotfiles"
 end
 
 function randpw --description "generate a random password"
