48bcb3509cce1f7cbcf72e903f35231db6057fd0
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-08 15:05:17 -0500

    init

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -7,21 +7,7 @@ declare current_dir && \
     cd "${current_dir}" && \
     source "$HOME/set-me-up/.dotfiles/utilities/utils.sh"
 
-readonly SMU_PATH="$HOME/set-me-up"
-readonly SMU_URL="https://github.com/nicholasadamou/set-me-up"
-
-declare LOCAL_BASH_CONFIG_FILE="${HOME}/.bash.local"
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-# Overrides `utils.sh` -> print_question() 
-# in order to add a few more spaces b/w '[?]'
-# & the left-most edge of the terminal window.
-print_question() {
-
-    print_in_yellow "     [?] $1"
-
-}
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 create_bash_local() {
 
@@ -38,7 +24,6 @@ create_bash_local() {
     fi
 
 }
-
 
 create_fish_local() {
 
@@ -66,10 +51,10 @@ create_gitconfig_local() {
 
         if [ "$(git -C "$SMU_PATH" config --global --get user.name)" = "" ] && [ "$(git -C "$SMU_PATH" config --global --get user.email)" = "" ]; then
             print_in_yellow "\n   Git Configuration\n\n"
-            
+
             ask "What is your name? [e.g. John Smith]: "; NAME="$(get_answer)"
             ask "What is your email address? [e.g. johnsmith@gmail.com]: "; EMAIL="$(get_answer)"
-            
+
             printf "\n"
         fi
 
@@ -107,79 +92,6 @@ create_vimrc_local() {
 
 }
 
-
-install_homebrew() {
-
-    printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" &> /dev/null
-    #       └─ simulate the ENTER keypress
-
-    print_result $? "Homebrew (install)" && \
-        add_brew_configs
-
-}
-
-add_brew_configs() {
-
-    declare -r BASH_CONFIGS="
-# Homebrew - The missing package manager for macOS.
-export PATH=\"/usr/local/bin:\$PATH\"
-export PATH=\"/usr/local/sbin:\$PATH\"
-"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # If needed, add the necessary configs in the
-    # local shell configuration file.
-
-    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$BASH_CONFIGS" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$BASH_CONFIGS' >> $LOCAL_BASH_CONFIG_FILE \
-                && . $LOCAL_BASH_CONFIG_FILE" \
-            "brew (update $LOCAL_BASH_CONFIG_FILE)"
-    fi
-
-}
-
-get_homebrew_git_config_file_path() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if path="$(brew --repository 2> /dev/null)/.git/config"; then
-        printf "%s" "$path"
-        return 0
-    else
-        print_error "Homebrew (get config file path)"
-        return 1
-    fi
-
-}
-
-opt_out_of_analytics() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Try to get the path of the `Homebrew` git config file.
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Opt-out of Homebrew's analytics.
-    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
-
-    if [ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
-    print_result $? "Homebrew (opt-out of analytics)"
-
-}
-
 symlink() {
 
     # Update and/or install dotfiles. These dotfiles are stored in the .dotfiles directory.
@@ -199,116 +111,28 @@ symlink() {
 
 }
 
-change_default_bash() {
-
-    local configs=""
-    local pathConfig=""
-
-    local newShellPath=""
-    local brewPrefix=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Try to get the path of the `Bash`
-    # version installed through `Homebrew`.
-
-    brewPrefix="$(brew --prefix)"
-
-    pathConfig="PATH=\"$brewPrefix/bin:\$PATH\""
-    configs="# Homebrew bash configurations
-$pathConfig
-export PATH"
-
-    newShellPath="$brewPrefix/bin/bash"
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Add the path of the `Bash` version installed through `Homebrew`
-    # to the list of login shells from the `/etc/shells` file.
-    #
-    # This needs to be done because applications use this file to
-    # determine whether a shell is valid (e.g.: `chsh` consults the
-    # `/etc/shells` to determine whether an unprivileged user may
-    # change the login shell for their own account).
-    #
-    # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
-
-    if ! grep -q "$(<<<"$newShellPath" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$newShellPath' | sudo tee -a /etc/shells" \
-            "Bash (add '$newShellPath' in '/etc/shells')"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Set latest version of `Bash` as the default
-    # (macOS uses by default an older version of `Bash`).
-
-    if [ "$(dscl . -read /Users/"${USER}"/ UserShell | cut -d ' ' -f2)" != "${newShellPath}" ]; then
-        chsh -s "$newShellPath" &> /dev/null
-        print_result $? "Bash (use latest version)"
-    else
-        print_success "(bash) is already on the latest version"
-    fi
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # If needed, add the necessary configs in the
-    # local shell configuration file.
-
-    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$configs" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
-        execute \
-            "printf '%s\n' '$configs' >> $LOCAL_BASH_CONFIG_FILE \
-                && . $LOCAL_BASH_CONFIG_FILE" \
-            "Bash (update $LOCAL_BASH_CONFIG_FILE)"
-    fi
-
-}
-
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 main() {
 
     print_in_purple "  Base\n"
 
+	apt_install_from_file "packages"
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     print_in_yellow "\n   Create local config files\n\n"
 
     create_bash_local
-    create_fish_local
+	create_fish_local
     create_gitconfig_local
     create_vimrc_local
-
-    print_in_yellow "\n   Homebrew\n\n"
-
-    ask_for_sudo
-
-    if ! cmd_exists "brew"; then
-        install_homebrew
-        opt_out_of_analytics
-    else
-        brew_upgrade
-        brew_update
-    fi
-
-    printf "\n"
-
-    brew_bundle_install "brewfile"
-
-    print_in_yellow "\n   Cleanup\n\n"
-
-    brew_cleanup
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     print_in_yellow "\n   Symlink dotfiles\n\n"
 
     symlink
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    print_in_yellow "\n   Upgrade bash\n\n"
-
-    change_default_bash
 
 }
 
