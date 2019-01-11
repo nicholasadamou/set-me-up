97ac901372e120b929cd28860e770626d8efd3c3
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-11 14:59:48 -0500

    updated

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -81,6 +81,12 @@ function obtain() {
 
 	curl --progress-bar -L "${download_url}" | tar -xz --strip-components 1 --exclude={README.md,LICENSE,.gitignore,.dotfiles/rcrc}
 
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	# If (nicholasadamou/set-me-up) has submodules
+	# make sure to install them prior to installing
+	# (nicholasadamou/dotfiles) submodules
+
 	if ! is_git_repo; then
 		git -C "${SMU_HOME_DIR}" init
 	fi
