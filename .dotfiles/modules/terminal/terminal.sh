0636baccad757c50c2cafa1b84d300f74ad73dd4
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-13 11:59:07 -0600

    updated terminal.sh

--- .dotfiles/modules/terminal/terminal.sh
+++ .dotfiles/modules/terminal/terminal.sh
@@ -9,36 +9,6 @@ declare current_dir && \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-# see: https://github.com/oh-my-fish/oh-my-fish/issues/189
-install_omf() {
-
-    if ! is_omf_installed; then
-        execute \
-            "curl -Ls github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > install && \
-                chmod +x install && \
-                ./install --noninteractive --path=$HOME/.local/share/omf --config=$HOME/.config/omf && \
-                 rm -rf install" \
-            "omf (install)"
-    else
-        print_success "(omf) is already installed."
-    fi
-
-}
-
-install_omf_packages() {
-
-    print_in_yellow "\n  Install omf packages\n\n"
-
-    omf_install "z"
-    omf_install "thefuck"
-    omf_install "spacefish"
-
-    printf "\n"
-
-    omf_update
-
-}
-
 install_fisher() {
 
     if ! is_fisher_installed; then
@@ -74,14 +44,6 @@ main() {
     brew_bundle_install "brewfile"
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    printf "\n"
-
-    ask_for_sudo
-
-    install_omf
-
-    install_omf_packages
 
     printf "\n"
 
