a998a1eae549893c54a20d4cc97558563ec1b5fd
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-27 16:00:04 -0600

    updated preferences.sh

--- .dotfiles/modules/preferences/preferences.sh
+++ .dotfiles/modules/preferences/preferences.sh
@@ -52,6 +52,7 @@ main() {
 	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 	# Restore Mac OS Preferences via `mackup`
+	# see: https://github.com/lra/mackup
 
 	command -v "mackup" &> /dev/null && [ -d "$HOME/Dropbox/Mackup" ] && {
 		execute \
