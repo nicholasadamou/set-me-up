4a9416df027c876f4d11cfa1994cbc6e4b6927ea
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-02-05 15:19:54 -0600

    added 'z.lua'

--- .dotfiles/tag-smu/config/fish/config.fish
+++ .dotfiles/tag-smu/config/fish/config.fish
@@ -25,6 +25,12 @@ if test -e "$HOME/.sdkman/bin/sdkman-init.sh"
     source "$SDKMAN_DIR/bin/sdkman-init.sh"
 end
 
+# load 'z.lua' configs.
+# see: https://github.com/skywind3000/z.lua#install
+if test -e "$HOME"/.z.lua
+    source (lua "$HOME"/.z.lua/z.lua --init fish | psub)
+end
+
 # Clear system messages (system copyright notice, the date
 # and time of the last login, the message of the day, etc.).
 
