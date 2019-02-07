b5d7d96bc9d6652415fec59aac4ee90dfec5a8c1
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-02-07 08:50:18 -0600

    updated install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -203,7 +203,7 @@ function setup() {
 
 				if [ -n "$files" ]; then
 					git -C "${SMU_HOME_DIR}" \
-						add "$files"
+						add -f "$files" &> /dev/null
 
 					git -C "${SMU_HOME_DIR}" \
 						-c user.name="set-me-up" \
