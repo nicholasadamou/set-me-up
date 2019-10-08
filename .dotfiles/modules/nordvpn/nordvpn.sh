456a51122f2eb496bbe019944b039b525df0c3c5
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-10-07 21:34:39 -0500

    added 'nordvpn' module

---
+++ .dotfiles/modules/nordvpn/nordvpn.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+
+declare current_dir && \
+    current_dir="$(dirname "${BASH_SOURCE[0]}")" && \
+    cd "${current_dir}" && \
+    source "$HOME/set-me-up/.dotfiles/utilities/utilities.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "  NordVPN\n\n"
+
+	apt_install_from_file "packages"
+
+}
+
+main
