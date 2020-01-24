4eafdc6115ab7c329e7664c684dab870dccbbfdc
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-01-24 10:03:16 -0600

    updated preferences

--- .dotfiles/modules/preferences/apps/alfred/alfred.sh
+++ .dotfiles/modules/preferences/apps/alfred/alfred.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# install alfred packages
-npm_install "awm"
+if brew info alfred &>/dev/null; then
+	# install alfred packages
+	npx_install "awm"
 
-[ -f "$(locate_alfred_preferences)" ] && {
-	npm_install "alfred-emoj"
-	npm_install "alfred-npms"
-	npm_install "alfred-dark-mode"
-	npm_install "alfred-cdnjs"
-	npm_install "alfred-packagist"
-	npm_install "alfred-mdi"
-	npm_install "alfred-awe"
-}
+	[ -f "$(locate_alfred_preferences)" ] && {
+		npx_install "alfred-emoj"
+		npx_install "alfred-npms"
+		npx_install "alfred-dark-mode"
+		npx_install "alfred-cdnjs"
+		npx_install "alfred-packagist"
+		npx_install "alfred-mdi"
+		npx_install "alfred-awe"
+	}
+fi
--- .dotfiles/modules/preferences/apps/terminal/terminal.sh
+++ .dotfiles/modules/preferences/apps/terminal/terminal.sh
@@ -11,9 +11,3 @@ defaults write com.apple.terminal StringEncodings -array 4
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
 ./set_terminal_theme.applescript
-
-# Ensure the Touch ID is used when `sudo` is required.
-
-if ! grep -q "pam_tid.so" "/etc/pam.d/sudo"; then
-    sudo sh -c 'echo \"auth sufficient pam_tid.so\" >> /etc/pam.d/sudo'
-fi
