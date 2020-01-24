2f326093f717554c2d122112cda16bae84aa807d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-01-24 10:31:27 -0600

    updated ui_and_ux.sh

--- .dotfiles/modules/preferences/system/ui_and_ux.sh
+++ .dotfiles/modules/preferences/system/ui_and_ux.sh
@@ -6,7 +6,7 @@ declare current_dir && \
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-declare -r DESKTOP_WALLPAPER_PATH="./wallpaper/wheat-field.jpeg"
+declare -r DESKTOP_WALLPAPER_PATH="${current_dir}/wallpaper/wheat-field.jpeg"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
