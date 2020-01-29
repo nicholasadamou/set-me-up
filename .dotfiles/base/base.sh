ec1e7567c0c14b208a0f7c6a28a83fabecaa104e
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-01-29 09:58:23 -0600

    rm terminal module

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -131,6 +131,125 @@ opt_out_of_analytics() {
 
 }
 
+change_default_bash() {
+
+    local configs=""
+    local pathConfig=""
+
+    local newShellPath=""
+    local brewPrefix=""
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Try to get the path of the `Bash`
+    # version installed through `Homebrew`.
+
+    brewPrefix="$(brew --prefix)"
+
+    pathConfig="PATH=\"$brewPrefix/bin:\$PATH\""
+    configs="# Homebrew bash configurations
+$pathConfig
+export PATH"
+
+    newShellPath="$brewPrefix/bin/bash"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Add the path of the `Bash` version installed through `Homebrew`
+    # to the list of login shells from the `/etc/shells` file.
+    #
+    # This needs to be done because applications use this file to
+    # determine whether a shell is valid (e.g.: `chsh` consults the
+    # `/etc/shells` to determine whether an unprivileged user may
+    # change the login shell for their own account).
+    #
+    # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
+
+    if ! grep -q "$(<<<"$newShellPath" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
+        printf '%s\n' "$newShellPath" | sudo tee -a /etc/shells
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Set latest version of `Bash` as the default
+    # (macOS uses by default an older version of `Bash`).
+
+    if [ "$(dscl . -read /Users/"${USER}"/ UserShell | cut -d ' ' -f2)" != "${newShellPath}" ]; then
+        chsh -s "$newShellPath" &> /dev/null
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # If needed, add the necessary configs in the
+    # local shell configuration file.
+
+    if [ ! -e "$LOCAL_BASH_CONFIG_FILE" ] || ! grep -q "$(<<<"$configs" tr '\n' '\01')" < <(less "$LOCAL_BASH_CONFIG_FILE" | tr '\n' '\01'); then
+        printf '%s\n' "$configs" >> "$LOCAL_BASH_CONFIG_FILE" \
+                && . "$LOCAL_BASH_CONFIG_FILE"
+    fi
+
+}
+
+create_fish_local() {
+
+    declare -r FILE_PATH="$HOME/.fish.local"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -e "$FILE_PATH" ] || [ -z "$FILE_PATH" ]; then
+        touch "$FILE_PATH"
+	fi
+
+}
+
+# see: https://github.com/oh-my-fish/oh-my-fish/issues/189
+install_omf() {
+
+    if ! is_omf_installed; then
+		# Make sure '$HOME/.local/share/omf' does not exist prior
+		# to 'omf' installation.
+
+		if [ -d "$HOME/.local/share/omf" ]; then
+			sudo rm -rf "$HOME/.local/share/omf"
+		fi
+
+		# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+        fish <(curl -Ls https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install) \
+				--noninteractive --yes --path="$HOME"/.local/share/omf --config="$HOME"/.config/omf
+    fi
+
+}
+
+install_omf_packages() {
+
+	omf_install "z"
+    omf_install "thefuck"
+
+    omf_update
+
+}
+
+install_fisher() {
+
+    if ! is_fisher_installed; then
+        curl -Lo "$HOME"/.config/fish/functions/fisher.fish --create-dirs --silent https://git.io/fisher
+    fi
+
+}
+
+install_fisher_packages() {
+
+	does_fishfile_exist && {
+        cat < "$HOME/.config/fish/fishfile" | while read -r PACKAGE; do
+            fisher_install "$PACKAGE"
+        done
+    }
+
+    fisher_update
+
+}
+
 symlink() {
 
     # Update and/or install dotfiles. These dotfiles are stored in the .dotfiles directory.
@@ -154,6 +273,9 @@ main() {
 
     create_bash_local
     create_gitconfig_local
+	create_fish_local
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     ask_for_sudo
 
@@ -170,6 +292,22 @@ main() {
     brew_cleanup
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	change_default_bash
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_omf
+
+    install_omf_packages
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_fisher
+
+    install_fisher_packages
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     symlink
 
