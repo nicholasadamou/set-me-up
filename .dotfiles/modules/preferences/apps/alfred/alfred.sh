e31ef75596cc2d65e8a2c74de479eea7e272dc0d
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-02-04 22:53:06 -0600

    fixed issue with [[ builtin

--- .dotfiles/modules/preferences/apps/alfred/alfred.sh
+++ .dotfiles/modules/preferences/apps/alfred/alfred.sh
@@ -4,7 +4,7 @@ if brew info alfred &>/dev/null; then
 	# install alfred packages
 	npx_install "awm"
 
-	[ -f "$(locate_alfred_preferences)" ] && {
+	[[ -f "$(locate_alfred_preferences)" ]] && {
 		npx_install "alfred-emoj"
 		npx_install "alfred-npms"
 		npx_install "alfred-dark-mode"
