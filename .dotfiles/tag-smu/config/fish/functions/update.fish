7173e5625935541bafcea7f574dfa2722635c505
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-03-21 14:58:18 -0400

    updated update command

--- .dotfiles/tag-smu/config/fish/functions/update.fish
+++ .dotfiles/tag-smu/config/fish/functions/update.fish
@@ -1,4 +1,11 @@
 function update --description "Updates Linux apps, brew, npm, fisher, omf update, pip, pip3 and their installed packages"
+   sudo apt update
+   sudo apt upgrade -y
+   sudo apt dist-upgrade -y
+   sudo apt full-upgrade -y
+   sudo apt autoremove -y
+   sudo apt clean
+
     if type -q brew
         brew update
         brew upgrade
