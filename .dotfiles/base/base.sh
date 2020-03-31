607b652c33615ed6f9250d10c6682903318aa97b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-03-31 04:48:56 -0400

    refactored a bit more

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -11,49 +11,6 @@ readonly SMU_PATH="$HOME/set-me-up"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_homebrew() {
-
-    printf "\n" | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-}
-
-get_homebrew_git_config_file_path() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    if path="$(brew --repository 2> /dev/null)/.git/config"; then
-        printf "%s" "$path"
-        return 0
-    else
-        return 1
-    fi
-
-}
-
-opt_out_of_analytics() {
-
-    local path=""
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Try to get the path of the `Homebrew` git config file.
-
-    path="$(get_homebrew_git_config_file_path)" \
-        || return 1
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    # Opt-out of Homebrew's analytics.
-    # https://github.com/Homebrew/brew/blob/0c95c60511cc4d85d28f66b58d51d85f8186d941/share/doc/homebrew/Analytics.md#opting-out
-
-    if [[ "$(git config --file="$path" --get homebrew.analyticsdisabled)" != "true" ]]; then
-        git config --file="$path" --replace-all homebrew.analyticsdisabled true &> /dev/null
-    fi
-
-}
-
 create_bash_local() {
 
     declare -r FILE_PATH="$HOME/.bash.local"
