7bc146382d5375661313afe06421d70c385da1c7
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-01 14:59:28 -0400

    Update install.sh

--- .dotfiles/modules/install.sh
+++ .dotfiles/modules/install.sh
@@ -311,7 +311,7 @@ function main() {
 	echo -e "\n${bold}\$HOME sweet /~\n${normal}"
 
 	echo -e "Welcome to the '${bold}set-me-up${normal}' installer."
-	echo -e "For more information, please see [https://github.com/nicholasadamou/set-me-up/tree/$SMU_VERSION]."
+	echo -e "For more information, please see [https://github.com/nicholasadamou/set-me-up]."
 	echo -e "Please follow the on-screen instructions.\n"
 
 	warn "${bold}This script sets up new machines, *use with caution*${normal}."
