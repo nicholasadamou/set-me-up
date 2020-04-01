02016c051e66fd132d491e9285375a6c60f7cff7
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-04-01 08:24:52 -0400

    fixed a few bugs

--- .dotfiles/base/base.sh
+++ .dotfiles/base/base.sh
@@ -46,7 +46,7 @@ change_default_bash_version() {
     # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
 
     if ! grep -q "$(<<<"$PATH_TO_HOMEBREW_BASH" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
-        printf '%s\n' "$PATH_TO_HOMEBREW_BASH" | sudo tee -a /etc/shells
+        printf '%s\n' "$PATH_TO_HOMEBREW_BASH" | sudo tee -a /etc/shells &> /dev/null
     fi
 
 }
@@ -75,6 +75,22 @@ change_default_shell_to_fish() {
 
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
+    # Add the path of the `fish` version installed through `Homebrew`
+    # to the list of login shells from the `/etc/shells` file.
+    #
+    # This needs to be done because applications use this file to
+    # determine whether a shell is valid (e.g.: `chsh` consults the
+    # `/etc/shells` to determine whether an unprivileged user may
+    # change the login shell for their own account).
+    #
+    # http://www.linuxfromscratch.org/blfs/view/7.4/postlfs/etcshells.html
+
+    if ! grep -q "$(<<<"$PATH_TO_FISH" tr '\n' '\01')" < <(less "/etc/shells" | tr '\n' '\01'); then
+        printf '%s\n' "$PATH_TO_FISH" | sudo tee -a /etc/shells &> /dev/null
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # Set latest version of `fish` as the default shell
 
     if [[ "$(dscl . -read /Users/"${USER}"/ UserShell | cut -d ' ' -f2)" != "${PATH_TO_FISH}" ]]; then
@@ -94,11 +110,13 @@ install_fisher() {
 
 install_fisher_packages() {
 
-	does_fishfile_exist && {
-        cat < "$HOME/.config/fish/fishfile" | while read -r PACKAGE; do
-            fisher_install "$PACKAGE"
-        done
-    }
+	local SMU_HOME="$SMU_PATH/.dotfiles/tag-smu"
+
+	# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+	cat < "$SMU_HOME/config/fish/fishfile" | while read -r PACKAGE; do
+		fisher_install "$PACKAGE"
+	done
 
     fisher_update
 
@@ -106,15 +124,17 @@ install_fisher_packages() {
 
 symlink() {
 
+	# Get the absolute path of the .dotfiles directory.
+    # This is only for aesthetic reasons to have an absolute symlink path instead of a relative one
+    # <path-to-smu>/.dotfiles/somedotfile vs <path-to-smu>/.dotfiles/base/../somedotfile
+    readonly dotfiles="${SMU_PATH}/.dotfiles"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
     # Update and/or install dotfiles. These dotfiles are stored in the .dotfiles directory.
     # rcup is used to install files from the tag-specific dotfiles directory.
     # rcup is part of rcm, a management suite for dotfiles.
     # Check https://github.com/thoughtbot/rcm for more info.
-
-    # Get the absolute path of the .dotfiles directory.
-    # This is only for aesthetic reasons to have an absolute symlink path instead of a relative one
-    # <path-to-smu>/.dotfiles/somedotfile vs <path-to-smu>/.dotfiles/base/../somedotfile
-    readonly dotfiles="${SMU_PATH}/.dotfiles"
 
     export RCRC="$dotfiles/rcrc" && \
             rcup -v -f -d "${dotfiles}"
