bc2452d0542ecc528fa73f0c409537480fa6d094
Nicholas Adamou <nicholasadamouemail@gmail.com>
2020-10-28 22:15:07 -0400

    fixed ordering of commands in config.fish

--- .dotfiles/tag-smu/config/fish/config.fish
+++ .dotfiles/tag-smu/config/fish/config.fish
@@ -39,13 +39,6 @@ if test -e "$HOME"/.z.lua
     source (lua "$HOME"/.z.lua/z.lua --init fish | psub)
 end
 
-# start tmux upon executing fish
-# see: https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332743061
-# if status is-interactive
-# and not set -q TMUX
-#    exec tmux
-# end
-
 # load starship prompt
 # see: https://starship.rs
 starship init fish | source
@@ -54,3 +47,10 @@ starship init fish | source
 # and time of the last login, the message of the day, etc.).
 
 clear
+
+# start tmux upon executing fish
+# see: https://github.com/fish-shell/fish-shell/issues/4434#issuecomment-332743061
+# if status is-interactive
+# and not set -q TMUX
+#    exec tmux
+# end
