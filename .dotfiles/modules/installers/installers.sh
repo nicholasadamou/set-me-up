212587f65e5140c9f503c62426a3932bc396df17
Nicholas Adamou <nicholasadamouemail@gmail.com>
2021-01-30 15:28:05 -0500

    added installers/, rm various apps from app_store/casks, update alfred preferences

---
+++ .dotfiles/modules/installers/installers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+
+declare current_dir && \
+    current_dir="$(dirname "${BASH_SOURCE[0]}")" && \
+    cd "${current_dir}" && \
+    source "$HOME/set-me-up/.dotfiles/utilities/utilities.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+	install_dmg "DMG/airbuddy-2.dmg"
+	install_dmg "DMG/WindowSwitcher.dmg"
+
+}
+
+main
